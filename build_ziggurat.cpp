0260719808675a1b7f70989802edb11b4f62e5c6
Alexandr Isaenkov <48714228+UnicornMane@users.noreply.github.com>
2019-11-07 00:53:36 +0300

    Update build_ziggurat.cpp

--- build_ziggurat.cpp
+++ build_ziggurat.cpp
@@ -1,11 +1,10 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-vector <vector <int>> build_ziggurat(const int &n)
+void build_ziggurat(const int &n, vector <vector <int>> &ziggurat)
 {
-	vector <vector <int>> ziggurat(n, vector <int> (n));
 	for (int k = 0; k < (n + 1) / 2; k++)
 	{
 		for (int i = k; i < n - k; i++)
@@ -28,14 +27,16 @@ vector <vector <int>> build_ziggurat(const int &n)
 			ziggurat[i][n - k - 1] = k + 1;
 		}
 	}
-	return ziggurat;
 }
 
 int main()
 {
 	int n;
 	cin >> n;
-	vector <vector <int>> ziggurat = build_ziggurat(n);
+	vector <vector <int>> ziggurat(n, vector <int> (n));
+
+	build_ziggurat(n, ziggurat);
+
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
