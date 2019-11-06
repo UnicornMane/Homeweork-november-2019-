607a33e5e5dda6c132415ad6f544f469f453aa0a
Alexandr Isaenkov <48714228+UnicornMane@users.noreply.github.com>
2019-11-06 23:32:30 +0300

    delete_duplicates

---
+++ delete_duplicates/delete_duplicates/delete_duplicates.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void delete_duplicates(int &size, vector <int> &arr)
+{
+	vector <bool> f(size, true);
+	vector <int> ans;
+	for (int i = 0; i < size; i++)
+	{
+		if (f[i])
+		{
+			ans.push_back(arr[i]);
+			for (int j = i + 1; j < size; j++)
+			{
+				if (arr[j] == arr[i])
+				{
+					f[j] = false;
+				}
+			}
+		}
+	} 
+	arr = ans;
+	size = ans.size();
+}
+
+int main()
+{
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+	vector <int> arr(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	delete_duplicates(n, arr);
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
