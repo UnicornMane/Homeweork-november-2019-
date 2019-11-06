7e79fa6e349574d9385d86a0c0fb2ba816dae57a
Alexandr Isaenkov <48714228+UnicornMane@users.noreply.github.com>
2019-11-07 00:15:12 +0300

    delete_annoying_primes

---
+++ delete_annoying_primes.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int sieve(size_t, int *s)
+{
+
+}
+
+void delete_annoying_primes(size_t size, int *arr)
+{
+
+}
+
+int main()
+{
+	size_t n;
+	cin >> n;
+
+	int *arr = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	delete_annoying_primes(n, arr);
+}
