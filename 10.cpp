63837454f031906df9819302cb927671226c9b2a
Alexandr Isaenkov <48714228+UnicornMane@users.noreply.github.com>
2019-11-07 01:37:33 +0300

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void cube_sum(const int& n, vector <int> &ans)
+{
+	if (n == 1)
+	{
+		ans.push_back(1);
+		return;
+	}
+	if (n == 0)
+	{
+		return;
+	}
+
+	int l = 0, r = n + 1;
+	while (r - l > 1)
+	{
+		int m = (r + l) / 2;
+		if (m * m * m <= n)
+		{
+			l = m;
+		} 
+		else
+			r = m;
+	}
+	ans.push_back(l);
+
+	cube_sum(n - l * l * l, ans);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector <int> ans;
+	
+	cube_sum(n, ans);
+
+	size_t ans_size = ans.size();
+
+	for (int i = 0; i < ans_size; i++)
+	{
+		cout << ans[i] << ' ';
+	}
+}
