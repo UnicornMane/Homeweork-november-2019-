63ca1a2270b7ce3d14297c0f83e49669d7de50a6
Alexandr Isaenkov <48714228+UnicornMane@users.noreply.github.com>
2019-11-08 15:44:48 +0300

    Add files via upload

---
+++ strange_sum.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int strange_sum(const int& f, const int& s, vector <int>& arr)
+{
+	if (s - f == 1)
+	{
+		return arr[f];
+	}
+	return strange_sum(f, (f + s) / 2, arr) + strange_sum((f + s) / 2, s, arr);
+
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector <int> arr(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	cout << strange_sum(0, n, arr);
+}
